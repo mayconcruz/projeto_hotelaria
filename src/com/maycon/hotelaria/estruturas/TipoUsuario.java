5b558afa59c4a9392161b0f36d3dcf6425624973
Maycon da Cruz Freiria <maycon-cruz@hotmail.com>
2016-06-29 08:49:30 -0300

    Modificações para adicionar Maven ao projeto

--- src/com/maycon/hotelaria/estruturas/TipoUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.maycon.hotelaria.estruturas;
-
-/**
- * A classe do tipo ENUM representa os possíveis níveis de usuário para acesso ao sistema
- */
-public enum TipoUsuario {
-    ADMINISTRADOR, COMUM
-
-}
